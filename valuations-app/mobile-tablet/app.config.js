526f37f17945dddf218755442b1986096ac3ba8b
Connor McLoughlin <connor.g.mcLoughlin@gmail.com>
2026-01-29 01:06:04 +0200

    Compile Fixes

--- valuations-app/mobile-tablet/app.config.js
+++ valuations-app/mobile-tablet/app.config.js
@@ -143,6 +143,7 @@ module.exports = {
         "foregroundImage": "./assets/images/adaptive-icon.png",
         "backgroundColor": "#ffffff"
       },
+      "compileSdkVersion": 35,
       "runtimeVersion": {
         "policy": "appVersion"
       },
